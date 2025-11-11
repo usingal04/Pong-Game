10e1804eb38b03915b190dfcb3e60ce15dfcf54a
Utsav Singal <62511146+usingal04@users.noreply.github.com>
2025-11-11 14:25:26 -0700

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-100, 180)
+        self.write(self.l_score, align='center', font=('Courier', 80, 'normal'))
+        self.goto(100, 180)
+        self.write(self.r_score, align='center', font=('Courier', 80, 'normal'))
+
+
+    def game_over(self):
+        self.goto(0, 0)
+        if self.l_score >= 10:
+            self.write('Game Over! Left Player Wins!', align='center', font=('Courier', 40, 'normal'))
+        else:
+            self.write('Game Over! Right Player Wins!', align='center', font=('Courier', 40, 'normal'))
