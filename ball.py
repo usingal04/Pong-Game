6385da6a5cc837466e5e0863775ce5b5d6f2ec4d
Utsav Singal <62511146+usingal04@users.noreply.github.com>
2025-11-11 14:23:03 -0700

    Update ball.py

--- ball.py
+++ ball.py
@@ -17,3 +17,10 @@ class Ball(Turtle):
 
     def bounce(self):
         self.y_move *= -1
+
+    def paddle_bounce(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.paddle_bounce()
